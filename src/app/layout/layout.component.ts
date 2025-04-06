f7781ce1cf2ec89cfd208638d4643e5f8b06b576
maobaid <maarijalobaid@gmail.com>
2025-04-06 11:42:17 +0300

    basics task done

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ContentComponent } from '../content/content.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent, ContentComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
